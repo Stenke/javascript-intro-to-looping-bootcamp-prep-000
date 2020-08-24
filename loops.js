7c7021312a440c5c7cf321412586b2b0b75ab0ce
Stephen Enke <stephenenke@gmail.com>
2020-08-24 19:22:18 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,13 +16,12 @@ function forLoop(array) {
     return 'done'
   }
   function doWhileLoop(num) {
-     num = num + 1;
-  return num;
-  }
+     function incrementVariable() {
+     i = i + 1;
+     return i;
+     }
   do {
     console.log("I run once regardless.");
-  } while (incrementVariable < num); {
-   function incrementVariable() {
-    i = i + 1;
-    return i;
-   }
+  } while (incrementVariable < num);
+   return incrementVariable;
+  }
